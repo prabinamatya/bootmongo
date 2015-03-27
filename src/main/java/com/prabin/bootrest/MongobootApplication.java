71737bceb881c399fdf138b6cfe1250945d5d96c
Prabin Amatya <pnamatya@gmail.com>
2015-03-27 15:30:19 -0500

    initial setup

---
+++ src/main/java/com/prabin/bootrest/MongobootApplication.java
@@ -0,0 +1,12 @@
+package com.prabin.bootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongobootApplication.class, args);
+    }
+}
---
+++ src/test/java/con/prabin/bootrest/MongobootApplicationTests.java
@@ -0,0 +1,18 @@
+package con.prabin.bootrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.prabin.bootrest.MongobootApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MongobootApplication.class)
+public class MongobootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
